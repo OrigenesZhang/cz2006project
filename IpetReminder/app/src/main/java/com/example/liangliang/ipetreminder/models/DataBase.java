3e8b117913683a6899df65adc90077a3707f23d8
CL <caoliu98@yahoo.com>
2018-04-09 10:58:10 +0800

    fix bugs

--- IpetReminder/app/src/main/java/com/example/liangliang/ipetreminder/models/DataBase.java
+++ IpetReminder/app/src/main/java/com/example/liangliang/ipetreminder/models/DataBase.java
@@ -32,18 +32,57 @@ public class DataBase {
         hygieneItems = new ArrayList<>();
         exerciseItems = new ArrayList<>();
 
-        medicineItems.add(new MedicineReminder("Vitamin", LocalTime.of(13,20), new GregorianCalendar(2018, Calendar.FEBRUARY, 12), new updateWithEndDate())),
-                new GregorianCalendar(2018, 4, 2), 2, Frequency.DAY));
-        medicineItems.add(new MedicineReminder("Antibiotic", LocalTime.of(15, 20), new GregorianCalendar(2018, Calendar.APRIL, 4),
-                new GregorianCalendar(2018, 7, 14), 4, Frequency.MONTH));
 
-        hygieneItems.add(new HygieneReminder("Bath",  2, Frequency.MONTH, new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 20)));
-        hygieneItems.add(new HygieneReminder("Hair",  3, Frequency.DAY, new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 21)));
-        hygieneItems.add(new HygieneReminder("Tooth",1, Frequency.WEEK, new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 2)));
+        medicineItems.add(
+                new MedicineReminder("Vitamin", LocalTime.of(13, 20),
+                        new GregorianCalendar(2018, Calendar.FEBRUARY, 12),
+                        new GregorianCalendar(2018, 4, 2),
+                        2, Frequency.DAY, new updateWithEndDate())
+        );
 
-        exerciseItems.add(new ExerciseReminder("Walk the dog", 1, Frequency.DAY, LocalTime.of(18, 20), new GregorianCalendar(2018, GregorianCalendar.APRIL, 1)));
-        exerciseItems.add(new ExerciseReminder("Ball game", 2, Frequency.WEEK, LocalTime.of(16, 30), new GregorianCalendar(2018, GregorianCalendar.MARCH, 1)));
-        exerciseItems.add(new ExerciseReminder("Swimming", 2, Frequency.MONTH, LocalTime.of(16,30), new GregorianCalendar(2018, GregorianCalendar.MAY, 1)));
+        medicineItems.add(
+                new MedicineReminder("Antibiotic", LocalTime.of(15, 20),
+                        new GregorianCalendar(2018, Calendar.APRIL, 4),
+                        new GregorianCalendar(2018, 7, 14),
+                        4, Frequency.MONTH, new updateWithEndDate())
+        );
+
+        hygieneItems.add(
+                new HygieneReminder("Bath",  2, Frequency.MONTH,
+                        new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 20),
+                        new updateWithoutEndDateWithoutTime())
+        );
+
+        hygieneItems.add(
+                new HygieneReminder("Hair",  3, Frequency.DAY,
+                        new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 21),
+                        new updateWithoutEndDateWithoutTime())
+        );
+
+        hygieneItems.add(
+                new HygieneReminder("Tooth",1, Frequency.WEEK,
+                        new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 2),
+                        new updateWithoutEndDateWithoutTime())
+        );
+
+        exerciseItems.add(
+                new ExerciseReminder("Walk the dog", 1, Frequency.DAY,
+                        LocalTime.of(18, 20),
+                        new GregorianCalendar(2018, GregorianCalendar.APRIL, 1),
+                        new updateWithoutEndDateWithTime())
+        );
+        exerciseItems.add(
+                new ExerciseReminder("Ball game", 2, Frequency.WEEK,
+                        LocalTime.of(16, 30),
+                        new GregorianCalendar(2018, GregorianCalendar.MARCH, 1),
+                        new updateWithoutEndDateWithTime())
+        );
+        exerciseItems.add(
+                new ExerciseReminder("Swimming", 2, Frequency.MONTH,
+                        LocalTime.of(16,30),
+                        new GregorianCalendar(2018, GregorianCalendar.MAY, 1),
+                        new updateWithoutEndDateWithTime())
+        );
 
     }
 
